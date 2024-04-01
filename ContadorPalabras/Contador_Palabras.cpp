b3a30f69f8ce05eec27af6e1bf6d7ffa6e946f31
Anderson Fernandez Bermudez <anderson@debian.operativos>
2024-04-01 01:17:00 -0500

    Contador de Palabras

---
+++ ContadorPalabras/Contador_Palabras.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+void ContarPalabras(){
+	int contador=0;
+	bool esPalabra=false;
+	int caracter=0;
+	while((caracter=getchar())!='\n'&&caracter!=EOF){
+		if((caracter==32)&&esPalabra){
+			esPalabra=false;
+			contador++;
+		}else{
+			if(caracter!=32){
+				esPalabra=true;
+			}
+		}
+
+	}
+	if(esPalabra){
+		contador++;
+	}
+	cout<<"La cantidad de palabras ingresadas es de: "<<contador<<endl;
+
+}
+int main(int argc,char *argv[]){
+	cout<<"Ingrese un texto para contar sus palabras: "<<endl;
+	ContarPalabras();
+	return 0;
+}
